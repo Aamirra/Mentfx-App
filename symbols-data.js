6ab8c9b8a365d17f79cfe2fd8731d94e1f2fc988
Aamirra <81755807+Aamirra@users.noreply.github.com>
2026-09-07 01:58:57 +0500

    Create symbols-data.js

---
+++ symbols-data.js
@@ -0,0 +1,31 @@
+// ========== SYMBOLS DATA ==========
+// Yahan apne symbols add/remove kar sakte hain
+
+const psxList = [
+    "SYS.PSX", "TRG.PSX", "AIRLINK.PSX", "WTL.PSX", "MEBL.PSX",
+    "UBL.PSX", "MCB.PSX", "HBL.PSX", "NBP.PSX", "OGDC.PSX",
+    "PPL.PSX", "MARI.PSX", "PSO.PSX", "FFC.PSX", "EFERT.PSX",
+    "ENGRO.PSX", "HUBC.PSX", "LUCK.PSX", "DGKC.PSX", "MLCF.PSX",
+    "FCCL.PSX", "CHCC.PSX", "PIOC.PSX", "PAEL.PSX", "MTL.PSX",
+    "INDU.PSX", "PSMC.PSX", "PRL.PSX", "CYAN.PSX", "EPCL.PSX",
+    "LOTCHEM.PSX", "COLG.PSX", "SEARL.PSX", "FEROZ.PSX", "INIL.PSX",
+    "ISL.PSX", "ASTL.PSX", "MUGHAL.PSX", "KOSM.PSX", "KEL.PSX",
+    "NPL.PSX", "NCPL.PSX", "NML.PSX", "ANL.PSX", "GATM.PSX",
+    "NESTLE.PSX", "FFL.PSX", "UNITY.PSX", "TREET.PSX", "SNGP.PSX",
+    "SSGC.PSX"
+];
+
+const cryptoList = [
+    "BTC-USD", "ETH-USD", "SOL-USD", "XRP-USD", "DOGE-USD",
+    "BNB-USD", "ADA-USD", "LINK-USD", "MATIC-USD", "AVAX-USD",
+    "DOT-USD"
+];
+
+const forexList = [
+    "EURUSD=X", "GBPUSD=X", "USDJPY=X", "USDCHF=X", "USDCAD=X",
+    "AUDUSD=X", "NZDUSD=X", "EURJPY=X", "GBPJPY=X"
+];
+
+const indicesList = [
+    "SPY", "QQQ", "DIA", "EWG", "EWU", "EWJ"
+];
